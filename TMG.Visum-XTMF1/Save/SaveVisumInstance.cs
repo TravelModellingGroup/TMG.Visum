dfd22b36e5dc11dee8aff68061c72a000551c74b
James Vaughan <James.Vaughan@utoronto.ca>
2023-06-19 13:32:13 -0400

    Implement Save Visum Instance (#6)

---
+++ TMG.Visum-XTMF1/Save/SaveVisumInstance.cs
@@ -0,0 +1,36 @@
+﻿namespace TMG.Visum.Save;
+
+[ModuleInformation(Description = "Save the current Visum instance to a file.")]
+public sealed class SaveVisumInstance : ISelfContainedModule
+{
+    [SubModelInformation(Required = true, Description = "The instance of Visum to operate on.")]
+    public IDataSource<VisumInstance> Visum = null!;
+
+    [SubModelInformation(Required = true, Description = "The path to save the Visum instance to.")]
+    public FileLocation SaveTo = null!;
+
+    public void Start()
+    {
+        var instance = Visum.LoadInstance();
+        try
+        {
+            instance.SaveVersionFile(SaveTo);
+        }
+        catch(Exception ex)
+        {
+            throw new XTMFRuntimeException(this, ex);
+        }
+    }
+
+    public bool RuntimeValidation(ref string? error)
+    {
+        return true;
+    }
+
+    public string Name { get; set; } = string.Empty;
+
+    public float Progress => 0f;
+
+    public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+
+}
---
+++ TMG.Visum-XTMF1/Utilities.cs
@@ -0,0 +1,21 @@
+﻿namespace TMG.Visum;
+
+/// <summary>
+/// Common operations
+/// </summary>
+internal static class Utilities
+{
+    /// <summary>
+    /// Get the visum instance or create a new one.
+    /// </summary>
+    /// <param name="source">The datasource containing the instance.</param>
+    /// <returns>A visum instance to operate on.</returns>
+    public static VisumInstance LoadInstance(this IDataSource<VisumInstance> source)
+    {
+        if (!source.Loaded)
+        {
+            source.LoadData();
+        }
+        return source.GiveData()!;
+    }
+}
