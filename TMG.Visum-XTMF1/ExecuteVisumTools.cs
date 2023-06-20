dd1b49cfb65f878c6e996021d8dd7dd834b88e9a
James Vaughan <James.Vaughan@utoronto.ca>
2023-06-20 10:01:14 -0400

    Implement XTMF1 tool versions of modules (#7)
    Also implemented LoadVersionFile to allow switching the version file after initializing.

---
+++ TMG.Visum-XTMF1/ExecuteVisumTools.cs
@@ -0,0 +1,52 @@
+﻿namespace TMG.Visum;
+
+[ModuleInformation(Description = "Allows to easilly chain together commands to a Visum instance.")]
+public sealed class ExecuteVisumTools : ISelfContainedModule
+{
+    [SubModelInformation(Required = true, Description = "The instance to use for all of the contained tools.")]
+    public IDataSource<VisumInstance> Instance = null!;
+
+    [SubModelInformation(Required = false, Description = "The tools to execute in order.")]
+    public IVisumTool[] Tools = null!;
+
+    private int _currentTool;
+
+    public void Start()
+    {
+        var instance = Instance.LoadInstance();
+        for (int i = 0; i < Tools.Length; i++)
+        {
+            _currentTool = i;
+            Tools[i].Execute(instance);
+        }
+        _currentTool = Tools.Length;
+    }
+
+    public bool RuntimeValidation(ref string? error)
+    {
+        return true;
+    }
+
+    public string Name { get; set; } = string.Empty;
+
+    public float Progress
+    {
+        get
+        {
+            if (Tools.Length <= 0
+                || _currentTool == Tools.Length)
+            {
+                return 1f;
+            }
+            var increment = 1.0f / Tools.Length;
+            return (_currentTool + MathF.Min(0, Tools[_currentTool].Progress)) * increment;
+        }
+    }
+
+    public Tuple<byte, byte, byte> ProgressColour => new(50, 50, 50);
+
+    public override string ToString()
+    {
+        return (Tools.Length <= 0 ? string.Empty : Tools[_currentTool].ToString()) ?? string.Empty;
+    }
+}
---
+++ TMG.Visum-XTMF1/Import/ImportMatrixToVisumTool.cs
@@ -0,0 +1,60 @@
+﻿namespace TMG.Visum.Import;
+
+[ModuleInformation(Description = "Store the given matrix into a Visum Instance.")]
+public sealed class ImportMatrixToVisumTool : IVisumTool
+{
+    [RunParameter("Matrix Number", 0, "The matrix number to store into.")]
+    public int MatrixNumber;
+
+    [RunParameter("Matrix Name", "", "The name to assosiate with the matrix.")]
+    public string MatrixName = string.Empty;
+
+    [SubModelInformation(Required = true, Description = "The matrix to Store into Visum")]
+    public IDataSource<SparseTwinIndex<float>> ToSave = null!;
+
+    public void Execute(VisumInstance instance)
+    {
+        try
+        {
+            if (!instance.TryGetMatrix(MatrixNumber, out var matrix))
+            {
+                matrix = instance.CreateDemandMatrix(MatrixNumber, MatrixName);
+            }
+            else
+            {
+                matrix.Name = MatrixName;
+            }
+            matrix.SetValues(GetMatrix());
+        }
+        catch (VisumException ex)
+        {
+            throw new XTMFRuntimeException(this, ex);
+        }
+    }
+
+    private float[][] GetMatrix()
+    {
+        var loaded = ToSave.Loaded;
+        if (!loaded)
+        {
+            ToSave.LoadData();
+        }
+        var ret = ToSave.GiveData();
+        if (!loaded)
+        {
+            ToSave.UnloadData();
+        }
+        return ret!.GetFlatData();
+    }
+
+    public bool RuntimeValidation(ref string? error)
+    {
+        return true;
+    }
+
+    public string Name { get; set; } = string.Empty;
+
+    public float Progress => 0f;
+
+    public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+}
