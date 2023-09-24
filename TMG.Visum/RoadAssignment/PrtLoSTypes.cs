6dc7ff21f3e93eecdf490a5c624cfe1bf28572e5
James Vaughan <James.Vaughan@utoronto.ca>
2023-09-24 10:14:03 -0400

    Add LoS calculation. (#21)

---
+++ TMG.Visum/RoadAssignment/PrtLoSTypes.cs
@@ -0,0 +1,138 @@
+﻿namespace TMG.Visum.RoadAssignment;
+
+/// <summary>
+/// The different type of matrices that can be skimmed
+/// </summary>
+/// <seealso cref="https://cgi.ptvgroup.com/vision-help/VISUM_2023_ENG/Content/1_Benutzermodell_IV/1_5_Kenngroessen_des_IV.htm"/>
+public enum PrTLosTypes
+{
+    /// <summary>
+    /// TSys-specific travel time t0 in unloaded network
+    /// </summary>
+    T0,
+
+    /// <summary>
+    /// TSys-specific travel time tCur in loaded network
+    /// </summary>
+    TCur,
+
+    /// <summary>
+    /// TSys-specific speed v0 in unloaded network
+    /// </summary>
+    V0,
+
+    /// <summary>
+    /// TSys-specific speed vCur in loaded network
+    /// </summary>
+    VCur,
+
+    /// <summary>
+    /// TSys-specific impedance in unloaded network
+    /// </summary>
+    Impedance,
+
+    /// <summary>
+    /// Distance covered from origin to destination
+    /// </summary>
+    TripDistance,
+
+    /// <summary>
+    /// Distance covered from origin to destination
+    /// </summary>
+    DirectDistance,
+
+    /// <summary>
+    /// Sum of AddValue 1
+    /// </summary>
+    AddValue1,
+
+    /// <summary>
+    /// Sum of AddValue 2
+    /// </summary>
+    AddValue2,
+
+    /// <summary>
+    /// Sum of AddValue 3
+    /// </summary>
+    AddValue3,
+
+    /// <summary>
+    /// Sum of TSys-AddValue data
+    /// </summary>
+    AddValueTSys,
+
+    /// <summary>
+    /// Toll of traversed links
+    /// </summary>
+    Toll,
+
+    /// <summary>
+    /// Flexible calculation of a mean attribute value per OD pair, also allows for the linkage
+    /// of attributes of different traversed network objects
+    /// </summary>
+    UserDefined,
+
+}
+
+/// <summary>
+/// Provides internal functions for working with PrTLoSTypes
+/// </summary>
+internal static class PrTLoSTypesHelper
+{
+    /// <summary>
+    /// Used for getting the name when building a SINGLESKIMMATRIXPARA
+    /// </summary>
+    /// <param name="type">The type to get the calculation name from.</param>
+    /// <returns>The string to be used when calculating the skim</returns>
+    /// <exception cref="VisumException">Thrown if the type is not valid.</exception>
+    public static string GetCalculationName(this PrTLosTypes type)
+    {
+        return type switch
+        {
+            PrTLosTypes.T0 => "T0",
+            PrTLosTypes.TCur => "TCUR",
+            PrTLosTypes.V0 => "V0",
+            PrTLosTypes.VCur => "VCUR",
+            PrTLosTypes.Impedance => "IMPEDANCE",
+            PrTLosTypes.TripDistance => "TRIPDIST",
+            PrTLosTypes.DirectDistance => "DIRECTDIST",
+            PrTLosTypes.AddValue1 => "ADDVAL1",
+            PrTLosTypes.AddValue2 => "ADDVAL2",
+            PrTLosTypes.AddValue3 => "ADDVAL3",
+            PrTLosTypes.AddValueTSys => "ADDVAL_TSYS",
+            PrTLosTypes.Toll => "TOLL",
+            PrTLosTypes.UserDefined => "USERDEF",
+            _ => throw new VisumException("Undefined PrTLoSType!")
+        };
+    }
+
+    /// <summary>
+    /// Computes the resulting matrix name given the demand segment
+    /// </summary>
+    /// <param name="type">The type of skim matrix.</param>
+    /// <param name="demandSegment">The demand segment used.</param>
+    /// <returns>Returns the expected name of the matrix after being calculated.</returns>
+    public static string GetMatrixName(this PrTLosTypes type, VisumDemandSegment demandSegment)
+    {
+        // We add a space at the end of each typeName to reduce the allocations.
+        var typeName = type switch
+        {
+            PrTLosTypes.T0 => "t0 ",
+            PrTLosTypes.TCur => "tCur ",
+            PrTLosTypes.V0 => "v0 ",
+            PrTLosTypes.VCur => "vCur ",
+            PrTLosTypes.Impedance => "Impedance ",
+            PrTLosTypes.TripDistance => "Trip distance ",
+            PrTLosTypes.DirectDistance => "Direct distance ",
+            PrTLosTypes.AddValue1 => "AddValue 1 ",
+            PrTLosTypes.AddValue2 => "AddValue 2 ",
+            PrTLosTypes.AddValue3 => "AddValue 3 ",
+            PrTLosTypes.AddValueTSys => "AddValue-Tsys ",
+            PrTLosTypes.Toll => "Toll ",
+            PrTLosTypes.UserDefined => "User-defined ",
+            _ => throw new VisumException("Undefined PrTLoSType!")
+        };
+        return typeName + demandSegment.Code;
+    }
+
+}
---
+++ TMG.Visum/VisumInstance/ExportRoadLoS.cs
@@ -0,0 +1,81 @@
+﻿// Ignore Spelling: Visum
+using TMG.Visum.RoadAssignment;
+namespace TMG.Visum;
+
+public partial class VisumInstance
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="demandSegment"></param>
+    /// <exception cref="VisumException"></exception>
+    public VisumMatrix CalculateRoadLoS(VisumDemandSegment demandSegment, PrTLosTypes type)
+    {
+        string? tempFileName = null;
+        try
+        {
+            _lock.EnterWriteLock();
+            ObjectDisposedException.ThrowIf(_visum is null, this);
+            tempFileName = WriteProcedure((writer) =>
+            {
+                /*
+                 <PRTSKIMMATRIXPARA DSEG="C" FILENAME="" FORMAT="V" ONLYRELATIONSWITHDEMAND="0"
+							   SEARCHCRITERION="Impedance" SELECTODRELATIONTYPE="All" SEPARATOR="Blank"
+							   SUMUPDESTCONNS="1" SUMUPLINKS="1" SUMUPORIGCONNS="1" SUMUPRESTRAFAREAS="1"
+							   SUMUPTURNS="1" USEEXISTINGPATHS="1" WEIGHTING="Route Vol Avg">
+                                    <SINGLESKIMMATRIXPARA CALCULATE="1" NAME="TCUR" SAVETOFILE="0"/>
+                 */
+                writer.WriteStartElement("OPERATION");
+                writer.WriteAttributeString("ACTIVE", "1");
+                writer.WriteAttributeString("NO", "1");
+                writer.WriteAttributeString("OPERATIONTYPE", "PrTSkimMatrixCalculation");
+                writer.WriteAttributeString("PARENTGROUPINDEX", "0");
+
+                writer.WriteStartElement("PRTSKIMMATRIXPARA");
+                writer.WriteAttributeString("DSEG", demandSegment.Code);
+                writer.WriteAttributeString("FORMAT", "V");
+                writer.WriteAttributeString("ONLYRELATIONSWITHDEMAND", "0");
+                writer.WriteAttributeString("SEARCHCRITERION", "Impedance");
+                writer.WriteAttributeString("SELECTODRELATIONTYPE", "All");
+                writer.WriteAttributeString("SEPARATOR", "Blank");
+                writer.WriteAttributeString("SUMUPDESTCONNS", "1");
+                writer.WriteAttributeString("SUMUPLINKS", "1");
+                writer.WriteAttributeString("SUMUPORIGCONNS", "1");
+                writer.WriteAttributeString("SUMUPRESTRAFAREAS", "1");
+                writer.WriteAttributeString("SUMUPTURNS", "1");
+                writer.WriteAttributeString("USEEXISTINGPATHS", "1");
+                writer.WriteAttributeString("WEIGHTING", "Route Vol Avg");
+
+                // Specify the particular matrix to compute
+                writer.WriteStartElement("SINGLESKIMMATRIXPARA");
+                writer.WriteAttributeString("CALCULATE", "1");
+                writer.WriteAttributeString("Name", type.GetCalculationName());
+                writer.WriteAttributeString("SAVETOFILE", "0");
+                // end SINGLESKIMMATRIXPARA
+                writer.WriteEndElement();
+
+                // end PRTSKIMMATRIXPARA
+                writer.WriteEndElement();
+                // end OPERATION
+                writer.WriteEndElement();
+
+            });
+            // Wipe out the previous procedures and run this.
+            _visum.Procedures.OpenXml(tempFileName, false);
+            _visum.Procedures.Execute();
+            return GetMatrixByNameInner(type.GetMatrixName(demandSegment));
+        }
+        catch(Exception ex)
+        {
+            throw new VisumException(ex);
+        }
+        finally
+        {
+            Files.SafeDelete(tempFileName);
+            _lock.ExitWriteLock();
+        }
+    }
+
+}
+
