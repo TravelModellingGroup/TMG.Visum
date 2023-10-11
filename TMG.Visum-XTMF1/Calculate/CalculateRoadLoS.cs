094e235981106a3fdf1c3691da04ed6ab42202c1
James Vaughan <James.Vaughan@utoronto.ca>
2023-10-11 11:48:27 -0400

    Added XTMF modules to calculate PrT matrices (#23)

---
+++ TMG.Visum-XTMF1/Calculate/CalculateRoadLoS.cs
@@ -0,0 +1,52 @@
+﻿using TMG.Visum.RoadAssignment;
+
+namespace TMG.Visum.Calculate;
+
+[ModuleInformation(Description =
+    "Generates the level of service matrices for the given types." +
+    " Will fail if there is no road assignment previously."
+)]
+public sealed class CalculateRoadLoS : IVisumTool
+{
+
+    [SubModelInformation(Required = true, Description = "The demand segment to calculate PrT for.")]
+    public DemandSegment Segment = null!;
+
+    [RunParameter("PrT Matrix Type", PrTLosTypes.TCur, "The type of matrix to compute from the previous road assignment.")]
+    public PrTLosTypes Type;
+
+    public void Execute(VisumInstance instance)
+    {
+        VisumDemandSegment? segment = null;
+        try
+        {
+            segment = GetSegment(instance);
+            instance.CalculateRoadLoS(segment, Type);
+        }
+        catch (VisumException e)
+        {
+            throw new XTMFRuntimeException(this, e);
+        }
+        finally
+        {
+            segment?.Dispose();
+        }
+    }
+
+    private VisumDemandSegment GetSegment(VisumInstance instance)
+    {
+        return instance.GetDemandSegment(Segment.Code);
+    }
+
+    public bool RuntimeValidation(ref string? error)
+    {
+        return true;   
+    }
+
+    public string Name { get; set; } = null!;
+
+    public float Progress => 0f;
+
+    public Tuple<byte, byte, byte> ProgressColour => new (50,150,50);
+
+}
---
+++ TMG.Visum-XTMF1/Common/DemandSegment.cs
@@ -0,0 +1,24 @@
+﻿// Ignore Spelling: visum
+
+namespace TMG.Visum;
+
+public class DemandSegment : IModule
+{
+    [RunParameter("Code", "C", "The code for the demand segment.")]
+    public string Code = null!;
+
+    [RunParameter("Demand Matrix", "", "The number of the matrix that will be used for demand.")]
+    public string DemandMatrix = null!;
+
+    public bool RuntimeValidation(ref string? error)
+    {
+        return true;
+    }
+
+    public string Name { get; set; } = null!;
+
+    public float Progress => 0f;
+
+    public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+}
+
