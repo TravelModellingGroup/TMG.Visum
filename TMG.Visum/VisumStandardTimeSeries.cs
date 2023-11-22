fc723f05cdcbf3d5aa9e6d797261ecef13c9262c
James Vaughan <James.Vaughan@utoronto.ca>
2023-11-22 15:57:53 -0500

    Implemented Demand and Standard Time Series (#28)

---
+++ TMG.Visum/VisumStandardTimeSeries.cs
@@ -0,0 +1,112 @@
+﻿
+namespace TMG.Visum;
+
+/// <summary>
+/// Provides access to a Standard Time Series
+/// </summary>
+public class VisumStandardTimeSeries : IDisposable
+{
+
+    /// <summary>
+    /// A reference to the underlying standard time series
+    /// </summary>
+    private ITimeSeries _timeSeries;
+
+    /// <summary>
+    /// The instance that the standard time series belongs to.
+    /// </summary>
+    private VisumInstance _instance;
+
+    internal VisumStandardTimeSeries(ITimeSeries timeSeries, VisumInstance instance)
+    {
+        _timeSeries = timeSeries;
+        _instance = instance;
+    }
+
+    /// <summary>
+    /// The name of the standard time series
+    /// </summary>
+    public string Name
+    {
+        get => (string)_timeSeries.AttValue["Name"];
+        set => _timeSeries.AttValue["Name"] = value;
+    }
+
+    /// <summary>
+    /// The number of the standard time series
+    /// </summary>
+    public int Number
+    {
+        get => (int)(double)_timeSeries.AttValue["No"];
+        set => _timeSeries.AttValue["No"] = (double)value;
+    }
+
+    /// <summary>
+    /// 0 = Time series of shares / percentage
+    /// 1 = Time series of matrices
+    /// </summary>
+    public int Type
+    {
+        get => (int)_timeSeries.AttValue["Type"];
+        set => _timeSeries.AttValue["Type"] = value;
+    }
+
+    /// <summary>
+    /// Get the time series item for the given
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public VisumTimeSeriesItem this[int index]
+    {
+        get
+        {
+            int current = 0;
+            // The key is a start time not the index in the series so we
+            // can't just get it by key.
+            foreach (ITimeSeriesItem item in _timeSeries.TimeSeriesItems)
+            {
+                if (current == index)
+                {
+                    return new VisumTimeSeriesItem(item, _instance);
+                }
+                current++;
+            }
+            throw new VisumException($"There was no Time Series Item at index {index}!");
+        }
+        
+    }
+
+    /// <summary>
+    /// The number of time series items within the time series
+    /// </summary>
+    public int Count
+    {
+        get => _timeSeries.TimeSeriesItems.Count;
+    }
+
+    ~VisumStandardTimeSeries()
+    {
+        Dispose(false);
+    }
+
+    private bool disposedValue;
+
+    private void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                COM.ReleaseCOMObject(ref _timeSeries!, false);
+                _instance = null!;
+            }
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ TMG.Visum/VisumTimeSeriesItem.cs
@@ -0,0 +1,215 @@
+﻿using VISUMLIB;
+
+namespace TMG.Visum;
+
+/// <summary>
+/// Represents an instance within a Time Series.
+/// </summary>
+public class VisumTimeSeriesItem : IDisposable
+{
+
+    /// <summary>
+    /// A reference to the underlying standard time series item
+    /// </summary>
+    private ITimeSeriesItem _timeSeriesItem;
+
+    /// <summary>
+    /// The instance that the standard time series belongs to.
+    /// </summary>
+    private VisumInstance _instance;
+
+    internal VisumTimeSeriesItem(ITimeSeriesItem timeSeriesItem, VisumInstance instance)
+    {
+        _timeSeriesItem = timeSeriesItem;
+        _instance = instance;
+    }
+
+    /// <summary>
+    /// Cumulative share up to this distribution interval.
+    /// </summary>
+    public double AccumulatedShare
+    {
+        get => (double)_timeSeriesItem.AttValue["AccumShare"];
+        set => _timeSeriesItem.AttValue["AccumShare"] = value;
+    }
+
+    /// <summary>
+    /// Number of the matrix resulting from the specified matrix reference based on the current data.
+    /// </summary>
+    public int CurrentMatrixNumber
+    {
+        get => (int)_timeSeriesItem.AttValue["CurrentMatrixNo"];
+    }
+
+    /// <summary>
+    /// Set the start and end times for the 
+    /// </summary>
+    /// <param name="startDate">The date the time series item should start at.</param>
+    /// <param name="startTime">The time that the time series item should start at in seconds.</param>
+    /// <param name="endDate">The date the time series item should end at.</param>
+    /// <param name="endTime">The time that the time series item should end at in seconds.</param>
+    public void SetTime(int? startDate, int startTime, int? endDate, int endTime)
+    {
+        // When we do this we can't have the start ever come before the 
+        var originalStart = (int)(double)_timeSeriesItem.AttValue["StartTime"];
+        // It is -1 because they start at 1
+        var newStartTime = (Math.Max(startDate - 1 ?? 0, 0) * 24 * 60 * 60) + startTime;
+        var newEndTime = (Math.Max((endDate - 1 ?? 0), 0) * 24 * 60 * 60) + endTime;
+        if(newEndTime < newStartTime)
+        {
+            throw new VisumException("The start time must be less than or equal to the end time.");
+        }
+        if(newStartTime < originalStart || newEndTime < originalStart)
+        {
+            _timeSeriesItem.AttValue["StartTime"] = (double)newStartTime;
+            _timeSeriesItem.AttValue["EndTime"] = (double)newEndTime;
+        }
+        else
+        {
+            _timeSeriesItem.AttValue["EndTime"] = (double)newEndTime;
+            _timeSeriesItem.AttValue["StartTime"] = (double)newStartTime;
+        }
+    }
+
+    /// <summary>
+    /// Time of the start time of this time series entry.
+    /// </summary>
+    public TimeOnly StartTimeInDay
+    {
+        // Internally this is represented as time in seconds within the day
+        get => TimeOnly.FromTimeSpan(TimeSpan.FromSeconds((double)_timeSeriesItem.AttValue["StartTimeInDay"]));
+        private set => _timeSeriesItem.AttValue["StartTimeInDay"] = value.ToTimeSpan().TotalSeconds;
+    }
+
+    /// <summary>
+    /// Time of the end time of this time series entry.
+    /// </summary>
+    public TimeOnly EndTimeInDay
+    {
+        // Internally this is represented as time in seconds within the day
+        get => TimeOnly.FromTimeSpan(TimeSpan.FromSeconds((double)_timeSeriesItem.AttValue["EndTimeInDay"]));
+        private set => _timeSeriesItem.AttValue["EndTimeInDay"] = value.ToTimeSpan().TotalSeconds;
+    }
+
+    /// <summary>
+    /// Gets the date component of the Start Time
+    /// </summary>
+    public int? StartDay
+    {
+        get
+        {
+            try
+            {
+                var day = (int?)(double?)_timeSeriesItem.AttValue["StartDay"];
+                return day;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private set
+        {
+            _timeSeriesItem.AttValue["StartDay"] = (double?)value;
+        }
+    }
+
+    /// <summary>
+    /// Gets the date component of the End Time
+    /// </summary>
+    public int? EndDay
+    {
+        get
+        {
+            try
+            {
+                var day = (int?)(double?)_timeSeriesItem.AttValue["EndDay"];
+                return day;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private set
+        {
+            _timeSeriesItem.AttValue["EndDay"] = (double?)value;
+        }
+    }
+
+    /// <summary>
+    /// The name of the demand matrix
+    /// </summary>
+    public string MatrixLabel
+    {
+        get => (string)_timeSeriesItem.AttValue["MatrixLabel"];
+    }
+
+    /// <summary>
+    /// The demand matrix for this time segment
+    /// </summary>
+    public VisumMatrix MatrixRef
+    {
+        get
+        {
+            if (_instance.Visum is not null)
+            {
+                return new VisumMatrix((IMatrix)_timeSeriesItem.AttValue["MatrixRef"], ObjectTypeRefT.OBJECTTYPEREF_ZONE, _instance.Visum);
+            }
+            throw new VisumException("The Visum instance was already disposed.");
+        }
+        set => _timeSeriesItem.AttValue["MatrixRef"] = value.Matrix;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public double Share
+    {
+        get => (double)_timeSeriesItem.AttValue["Share"];
+    }
+
+    /// <summary>
+    /// Number of the time series.
+    /// </summary>
+    public int TimeSeriesNumber
+    {
+        get => (int)_timeSeriesItem.AttValue["TimeSeriesNo"];
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public double Weight
+    {
+        get => (double)_timeSeriesItem.AttValue["Weight"];
+        set => _timeSeriesItem.AttValue["Weight"] = value;
+    }
+
+    ~VisumTimeSeriesItem()
+    {
+        Dispose(false);
+    }
+
+    private bool disposedValue;
+
+    private void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                COM.ReleaseCOMObject(ref _timeSeriesItem!, false);
+                _instance = null!;
+            }
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+}
