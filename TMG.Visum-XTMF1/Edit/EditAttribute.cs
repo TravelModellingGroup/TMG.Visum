79d96c8bf7bef5d209275326cdf5c0e6b523d222
James Vaughan <James.Vaughan@utoronto.ca>
2023-11-25 13:43:39 -0500

    Implement EditAttribute (#32)

---
+++ TMG.Visum-XTMF1/Edit/EditAttribute.cs
@@ -0,0 +1,51 @@
+﻿namespace TMG.Visum.Edit;
+
+[ModuleInformation(
+    Description = "Executes an EditAttribute procedure on the current VISUM instance."
+    )]
+public sealed class EditAttribute : IVisumTool
+{
+    [RunParameter("Formula", "", "The formula to apply.")]
+    public string Formula = null!;
+
+    [RunParameter("Network Object Type", "LINK", "The name of the network object.")]
+    public string NetObjectType = null!;
+
+    [RunParameter("Result Attribute Name", "", "The name of the attribute to store the result into.")]
+    public string ResultAttributeName = null!;
+
+    [RunParameter("Only Active", false, "Should we assign to all network objects of the type or only the active ones?")]
+    public bool OnlyActive;
+
+    public void Execute(VisumInstance visumInstance)
+    {
+        visumInstance.RunEditAttribute(Formula,
+            NetObjectType, ResultAttributeName, OnlyActive);
+    }
+
+    public bool RuntimeValidation(ref string? error)
+    {
+        if(string.IsNullOrWhiteSpace(Formula))
+        {
+            error = "The formula must be non-empty!";
+            return false;
+        }
+        if (string.IsNullOrWhiteSpace(NetObjectType))
+        {
+            error = "The Network Object Type must be non-empty!";
+            return false;
+        }
+        if (string.IsNullOrWhiteSpace(ResultAttributeName))
+        {
+            error = "The Result Attribute Name must be non-empty!";
+            return false;
+        }
+        return true;
+    }
+
+    public string Name { get; set; } = null!;
+
+    public float Progress => 0f;
+
+    public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+}
--- TMG.Visum/VisumInstance/EditAttribute.cs
+++ TMG.Visum/VisumInstance/EditAttribute.cs
@@ -2,8 +2,56 @@
 
 public partial class VisumInstance
 {
-    public void RunEditAttribute()
+    /// <summary>
+    /// Execute an EditAttribute command
+    /// </summary>
+    /// <param name="formula">The formula to apply.</param>
+    /// <param name="netObjectType">The network object type name to assign to.</param>
+    /// <param name="resultAttributeName">The attribute name to assign to.</param>
+    /// <param name="onlyActive">Should we only edit actively selected objects?</param>
+    public void RunEditAttribute(string formula, string netObjectType, string resultAttributeName, bool onlyActive)
     {
+        _lock.EnterWriteLock();
+        string? tempFileName = null;
+        try
+        {
+            ObjectDisposedException.ThrowIf(_visum is null, this);
+            tempFileName = WriteProcedure((writer) =>
+            {
+                writer.WriteStartElement("OPERATION");
+                writer.WriteAttributeString("ACTIVE", "1");
+                writer.WriteAttributeString("NO", "1");
+                writer.WriteAttributeString("OPERATIONTYPE", "EditAttribute");
+                writer.WriteAttributeString("PARENTGROUPINDEX", "0");
 
+                writer.WriteStartElement("ATTRIBUTEFORMULAPARA");
+
+                writer.WriteAttributeString("FORMULA", formula);
+                writer.WriteAttributeString("INCLUDESUBCATEGORIES", "0");
+                writer.WriteAttributeString("NETOBJECTTYPE", netObjectType);
+                writer.WriteAttributeString("ONLYACTIVE", onlyActive ? "1" : "0");
+                writer.WriteAttributeString("RESULTATTRNAME", resultAttributeName);
+
+                // end ATTRIBUTEFORMULAPARA
+                writer.WriteEndElement();
+            });
+            // Wipe out the previous procedures and run this.
+            _visum.Procedures.OpenXmlWithOptions(tempFileName);
+            _visum.Procedures.Execute();
+        }
+        catch (VisumException)
+        {
+            // Just pass through VisumExceptions
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new VisumException(ex);
+        }
+        finally
+        {
+            Files.SafeDelete(tempFileName);
+            _lock.ExitWriteLock();
+        }
     }
 }
