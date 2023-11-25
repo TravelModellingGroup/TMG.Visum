d778b798bc60c0382349f1a68df61b9270b690da
James Vaughan <phibred@hotmail.com>
2023-11-25 13:43:10 -0500

    Implement EditAttribute

---
+++ TMG.Visum.Test/TestEditAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace TMG.Visum.Test;
+
+[TestClass]
+public class TestEditAttribute
+{
+
+    [TestMethod]
+    public void EditAttribute()
+    {
+        using var instance = new VisumInstance("TestNetwork.ver");
+        instance.RunEditAttribute("123", "LINK", "LENGTH", false);
+        // TODO: Figure out how to automatically detect an error.
+        // instance.SaveVersionFile("out.ver");
+    }
+
+}
