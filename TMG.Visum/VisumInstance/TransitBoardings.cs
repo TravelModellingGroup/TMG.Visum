ebdfbe1d44710125e8fea95afeb69e3c5f40df6d
James Vaughan <phibred@hotmail.com>
2024-02-03 18:45:21 -0500

    Improved GetBoardings performance

--- TMG.Visum/VisumInstance/TransitBoardings.cs
+++ TMG.Visum/VisumInstance/TransitBoardings.cs
@@ -1,34 +1,48 @@
-﻿namespace TMG.Visum;
+﻿using System.Runtime.InteropServices;
+
+namespace TMG.Visum;
 
 public partial class VisumInstance
 {
 
+    /// <summary>
+    /// Get the boards by line.
+    /// This method will overwrite ADDVAL1 for the Lines.
+    /// </summary>
+    /// <returns>
+    ///     A list of the transit line names with the 
+    ///     corresponding sum of passenger boardings.
+    /// </returns>
+    /// <exception cref="VisumException">Throws if there is an error connecting to the VISUM server.</exception>
     public List<(string lineName, float boardings)> GetBoardings()
     {
-        _lock.EnterReadLock();
+        _lock.EnterWriteLock();
         try
         {
             ObjectDisposedException.ThrowIf(_visum is null, this);
-
             var ret = new List<(string lineName, float boardings)>();
-            double boardings = 0.0;
+            ExecuteEditAttributeInternal(new EditAttributeParameters()
+            {
+                NetObjectType = "LINE",
+                Formula = "[SUMACTIVE:LINEROUTES\\SUMACTIVE:LINEROUTEITEMS\\PASSBOARD(AP)]",
+                OnlyActive = true,
+                ResultAttributeName = "ADDVAL1"
+            });
             foreach(ILine line in _visum.Net.Lines)
             {
-                foreach(ILineRoute route in line.LineRoutes)
-                {
-                    foreach(ILineRouteItem item in route.LineRouteItems)
-                    {
-                        boardings += item.GetBoardings();
-                    }
-                }
+                double boardings = 0.0;
+                boardings = (double)line.AttValue["ADDVAL1"];
                 ret.Add((line.GetName(), (float)boardings));
             }
-
             return ret;
+        }
+        catch(COMException e)
+        {
+            throw new VisumException(e);
         }
         finally
         {
-            _lock.ExitReadLock();
+            _lock.ExitWriteLock();
         }
     }
 
